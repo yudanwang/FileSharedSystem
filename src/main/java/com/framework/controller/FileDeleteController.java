35fedce4f6ef70bfb2773f2b3e48d0c881a2292a
Yudan Wang <yudanwang12@gmail.com>
2016-03-19 19:13:01 +0100

    add filedownload and fileupdate

--- src/main/java/com/framework/controller/FileDeleteController.java
+++ src/main/java/com/framework/controller/FileDeleteController.java
@@ -8,19 +8,10 @@ import com.framework.model.ArtifactEntity;
 import com.framework.repository.ArtifactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class FileDeleteController {
@@ -35,7 +26,7 @@ public class FileDeleteController {
         String name = artifactEntity.getName();
         File deletefile = new File("D:/" + name);
         deletefile.delete();
-        
+
         artifactRepository.delete(artifactId);
         artifactRepository.flush();
         return "redirect:/artifact";
